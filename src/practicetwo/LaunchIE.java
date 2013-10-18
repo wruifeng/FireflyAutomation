c3632486566b84a6f17a80bd0fd0cc2b284e9ddc
terry <your_sky@126.com>
2013-10-18 13:26:14 +0800

    Add ie deal with cert issue.

--- src/practicetwo/LaunchIE.java
+++ src/practicetwo/LaunchIE.java
@@ -7,28 +7,29 @@ import org.testng.annotations.*;
 
 public class LaunchIE {
 	
-	private WebDriver iewb = null;
-	private DesiredCapabilities caps = null;
+	private static WebDriver iewb = null;
+	private static DesiredCapabilities caps = null;
 	private String projectpath = System.getProperty("user.dir");
 	
 	
 	@BeforeClass
 	public void startIE(){
-		System.setProperty("webdriver.ie.driver", projectpath+"/tool/IEDriverServer64.exe"); 
+		System.setProperty("webdriver.ie.driver", projectpath+"/tool/IEDriverServer32.exe"); 
 		caps = DesiredCapabilities.internetExplorer();
 		caps.setCapability(InternetExplorerDriver.FORCE_CREATE_PROCESS, false);
+		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);   
 		caps.setCapability(InternetExplorerDriver.IE_SWITCHES, "-private");		
         caps.setCapability("ignoreZoomSetting", true);
-        
+        iewb = new InternetExplorerDriver(caps);
         
 	}
 	
 
 	
 	@Test
-	public void searchOnBaidu(){
-		iewb = new InternetExplorerDriver(caps);
-		iewb.get("http://www.baidu.com");
+	public void searchOnBaidu(){	
+		iewb.get("https://login.providerlink.healthcare.stg.covisint.com/providerlink");
+		iewb.get("javascript:document.getElementById('overridelink').click();");
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
