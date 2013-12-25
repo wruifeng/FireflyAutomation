4d790af8eecc0ef1646293713dc43c244c003ec6
AlwaysTerry <admin123>
2013-12-25 15:02:28 +0800

    mm

---
+++ src/practiceten_1/TestngXlst.java
@@ -0,0 +1,57 @@
+package practiceten_1;
+
+import org.testng.annotations.Test;
+
+public class TestngXlst {
+
+	@Test
+	public void test1(){
+		
+	}
+	
+	@Test
+	public void test2(){
+		
+	}
+	
+	@Test
+	public void test3(){
+		
+	}
+	
+	
+	@Test
+	public void test4(){
+		
+	}
+	
+	@Test
+	public void test5(){
+		
+	}
+	
+	@Test
+	public void test6(){
+		
+	}
+	
+	@Test
+	public void test7(){
+		
+	}
+	
+	@Test
+	public void test8(){
+		
+	}
+	
+	@Test
+	public void test9(){
+		
+	}
+	
+	@Test
+	public void test10(){
+		
+	}
+}
