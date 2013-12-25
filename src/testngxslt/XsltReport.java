fba6bf43839f44165c006e090d74d0a354fe4126
AlwaysTerry <admin123>
2013-12-25 14:22:07 +0800

    jj

---
+++ src/testngxslt/XsltReport.java
@@ -0,0 +1,54 @@
+package testngxslt;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class XsltReport {
+	private WebDriver ffwb = null;
+	private FirefoxProfile firefoxprofile = null;
+	private String projectpath = System.getProperty("user.dir");
+	
+	
+	@BeforeClass
+	public void startFirefox(){
+		File firebug = new File(projectpath+"/tool/firebug-1.12.1-fx.xpi");
+	    File firepath = new File(projectpath+"/tool/firepath-0.9.7-fx.xpi");
+		firefoxprofile =  new FirefoxProfile();
+		try {
+			firefoxprofile.addExtension(firebug);
+			firefoxprofile.addExtension(firepath);
+			firefoxprofile.setPreference("webdriver.accept.untrusted.certs", "true"); 
+			firefoxprofile.setPreference("extensions.firebug.currentVersion", "1.12.1");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	
+	
+	@Test
+	public void searchOnBaidu(){
+		ffwb = new FirefoxDriver(firefoxprofile);
+		ffwb.get("http://www.baidu.com");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterClass
+	public void releaseFFDriver(){
+		ffwb.quit();
+	}
+	
+}
